0ea2507e9fec9675f29eb516ad97a441895b0ce6
Cryingabriel <Cai19mac@gmail.com>
2023-11-13 15:15:04 -0700

    data structures slide 12

--- ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -4,6 +4,7 @@ using namespace std;
 string Laundry[20];
 int top = -1;
 int top1 = 9;
+bool stop = false;
 
 void bluepush(string clothing) {
     if (top >= 9)
@@ -89,13 +90,60 @@ void redsize() {
 
 int main()
 {
-    bluepush("lol");
-    redpush("ahh");
-    redpush("pop");
 
+    while (stop == false)
+    {
+        int choice;
+        string choice2;
+        int choice3;
+        cout << "what do you wish to do? 1) push, 2) pop, 3) display, 4) size, or 5 quit" << endl;
+        cin >> choice;
+        switch (choice) {
+            case 1:
+                cout << "what do you wish to type?" << endl;
+                cin >> choice2;
+                cout << "which stack do you wish to push it in? 1) blue or 2) red" << endl;
+                cin >> choice3;
+                if (choice3 == 1) {
+                    bluepush(choice2);
+                }
+                else if (choice3 == 2) {
+                    redpush(choice2);
+                }
+                break;
+            case 2:
+                cout << "which stack do you wish to pop? 1) blue or 2) red" << endl;
+                cin >> choice3;
+                if (choice3 == 1) {
+                    bluepop();
+                }
+                else if (choice3 == 2) {
+                    redpop();
+                }
+                break;
+            case 3:
+                cout << "which stack do you wish to display? 1) blue or 2) red" << endl;
+                cin >> choice3;
+                if (choice3 == 1) {
+                    bluedisplay();
+                }
+                else if (choice3 == 2) {
+                    reddisplay();
+                }
+                break;
+            case 4:
+                cout << "which stack do you wish to see the size of? 1) blue or 2) red" << endl;
+                cin >> choice3;
+                if (choice3 == 1) {
+                    bluesize();
+                }
+                else if (choice3 == 2) {
+                    redsize();
+                }
+                break;
+            case 5:
+                stop = true;
+            }
 
-    redsize();
-    reddisplay();
-    bluesize();
-    bluedisplay();
+    }
 }
